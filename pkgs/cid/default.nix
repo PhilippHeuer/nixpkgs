93ef81d91f0b62b6527abf7f23d6af6c1d84e3ff
Philipp Heuer <git@philippheuer.me>
2024-05-12 23:47:09 +0200

    feat: add cid

---
+++ pkgs/cid/default.nix
@@ -0,0 +1,52 @@
+{ lib
+, stdenv
+, installShellFiles
+, fetchFromGitHub
+, buildGoModule
+}:
+
+buildGoModule rec {
+  pname = "cid";
+  version = "v0.1.0";
+
+  src = fetchFromGitHub {
+    owner = "cidverse";
+    repo = "cid";
+    rev = "71574ddd6cd05f7b9592706642f931d262f9121c"; # version
+    sha256 = "sha256-1ZZpqCYvzu+8EPFVu9KtZaprz2p183EuNxBjp2zR2RM=";
+  };
+  vendorHash = "sha256-51MQSEz1YcO5079wH7d22SG16+XQQkdi3e/EKYhzf2g=";
+
+  ldflags = [
+    "-X main.Version=${version}"
+    "-X main.CommitHash=none"
+    "-X main.RepositoryStatus=clean"
+    "-X main.BuildAt=none"
+  ];
+
+  nativeBuildInputs = [
+    installShellFiles
+  ];
+
+  # disable checks
+  doCheck = false;
+
+  # completions
+  postInstall = ''
+      # install shell completion
+      installShellCompletion --cmd cid \
+        --bash <($out/bin/cid completion bash) \
+        --fish <($out/bin/cid completion fish) \
+        --zsh  <($out/bin/cid completion zsh)
+    '';
+
+  # metadata
+  meta = with lib; {
+    homepage = "https://github.com/cidverse/cid";
+    description = "cid cli";
+    license = licenses.mit;
+    platforms = platforms.unix;
+    maintainers = with maintainers; [ ];
+    mainProgram = "cid";
+  };
+}
