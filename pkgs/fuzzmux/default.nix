3a55baeb77147d5da05161227d9ce5e8ddb50a9e
Philipp Heuer <git@philippheuer.me>
2024-11-06 21:10:16 +0100

    feat: bump fuzzmux to v0.8.0

--- pkgs/fuzzmux/default.nix
+++ pkgs/fuzzmux/default.nix
@@ -3,19 +3,20 @@
 , installShellFiles
 , fetchFromGitHub
 , buildGoModule
+, buildGo123Module
 }:
 
-buildGoModule rec {
+buildGo123Module rec {
   pname = "fuzzmux";
-  version = "v0.7.0";
+  version = "v0.8.0";
 
   src = fetchFromGitHub {
     owner = "PhilippHeuer";
     repo = "fuzzmux";
-    rev = version;
-    sha256 = "sha256-9Qk4SUHmxXiJ8Vy2CDToDeVLnFvaWAtwNFi0cJXPU8c=";
+    rev = "5698dca39e49118d6c15cd223e4c9d9ba61a5c1c";
+    sha256 = "sha256-nHXF3smeKljuakWz5gskCg1B70KpYTZXlkG38tBIPzU=";
   };
-  vendorHash = "sha256-tAWjYuBVO9ux2nnHwNPZoDr9Mx9a7YfqOBgJK12IdYI=";
+  vendorHash = "sha256-mxsVEu8Jby9I7zTLtl33Fp8VsQBHWKMA6kC5FiQcox4=";
 
   ldflags = [
     "-s"
