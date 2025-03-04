b36a5c74f47382a2c8fc729052aeba0dd8c3aabe
Philipp Heuer <git@philippheuer.de>
2025-03-04 21:44:23 +0100

    feat: add speakeasy pkg, fix cryptomator

--- default.nix
+++ default.nix
@@ -31,6 +31,7 @@
   fossa-cli = pkgs.callPackage ./pkgs/fossa-cli { };
   sarifrs = pkgs.callPackage ./pkgs/sarifrs { };
   cryptomator-cli = pkgs.callPackage ./pkgs/cryptomator-cli { };
+  speakeasy = pkgs.callPackage ./pkgs/speakeasy { };
   goland-eap = pkgs.callPackage ./pkgs/jetbrains/goland-eap { };
   idea-ultimate-eap = pkgs.callPackage ./pkgs/jetbrains/idea-ultimate-eap { };
 }
