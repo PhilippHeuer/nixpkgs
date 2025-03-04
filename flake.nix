ac3a5c21e90b3604e9379421b10b7c86c410792e
Philipp Heuer <git@philippheuer.de>
2025-03-04 01:34:34 +0100

    feat: add idea-ultimate and goland eap packagess

--- flake.nix
+++ flake.nix
@@ -18,7 +18,10 @@
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
   in {
     legacyPackages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
       });
       packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
   };
