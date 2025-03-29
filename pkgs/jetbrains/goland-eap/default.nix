21ab315710d197fd7a4d9e915d45dd99d44a7cda
Philipp Heuer <git@philippheuer.de>
2025-03-29 14:17:06 +0100

    feat: update jetbrains ides

--- pkgs/jetbrains/goland-eap/default.nix
+++ pkgs/jetbrains/goland-eap/default.nix
@@ -5,8 +5,8 @@
 (pkgs.jetbrains.goland.overrideAttrs {
     version = "2025.1 Beta";
     src = pkgs.fetchurl {
-        url = "https://download-cdn.jetbrains.com/go/goland-251.23774.41.tar.gz";
-        sha256 = "9c69e2b8d40fb27baf397e4d50e07e7525e9e8f3af6d6afc29f49a102da2f994";
+        url = "https://download-cdn.jetbrains.com/go/goland-251.23774.216.tar.gz";
+        sha256 = "9c74be72b091a866d35b3561f697c4161750dee8350e7b2f3563b6012c9333d0";
     };
-    build_number = "251.23774.41";
+    build_number = "251.23774.216";
 })
