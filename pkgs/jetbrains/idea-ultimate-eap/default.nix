0057ac4c7e1fb8d9b08b7ab4a20ca139c46ecd96
Philipp Heuer <git@philippheuer.de>
2025-03-29 14:17:06 +0100

    feat: update jetbrains ides

--- pkgs/jetbrains/idea-ultimate-eap/default.nix
+++ pkgs/jetbrains/idea-ultimate-eap/default.nix
@@ -5,8 +5,8 @@
 (pkgs.jetbrains.idea-ultimate.overrideAttrs {
     version = "2025.1 Beta";
     src = pkgs.fetchurl {
-        url = "https://download-cdn.jetbrains.com/idea/ideaIU-251.23774.16.tar.gz";
-        sha256 = "8ad3410cb3865976bead7e4a1f82e25b96cf32d71b1a570965e3d1d293933196";
+        url = "https://download-cdn.jetbrains.com/idea/ideaIU-251.23774.200.tar.gz";
+        sha256 = "7ce57055e3a5ee92582eb90c2dea469b48a6e7165e74fe085ef1923c16ba7faa";
     };
-    build_number = "251.23774.16";
+    build_number = "251.23774.200";
 })
--- pkgs/jetbrains/rider-eap/default.nix
+++ pkgs/jetbrains/rider-eap/default.nix
@@ -3,10 +3,10 @@
 }:
 
 (pkgs.jetbrains.rider.overrideAttrs {
-    version = "2025.1 EAP 7";
+    version = "2025.1 EAP 9";
     src = pkgs.fetchurl {
-        url = "https://download-cdn.jetbrains.com/rider/JetBrains.Rider-2025.1-EAP7-251.23774.20.Checked.tar.gz";
-        sha256 = "37c1c9c9237a513e62dacb474efd9beba9cd5d2024621a26f0fbd52eb736f2c9";
+        url = "https://download-cdn.jetbrains.com/rider/JetBrains.Rider-2025.1-EAP9-251.23774.212.Checked.tar.gz";
+        sha256 = "60db3a6cf5e4bd2e6cbaabcf3d53ebad81c6e4e16ced89414cabf5468fef6b6e";
     };
-    build_number = "251.23774.20";
+    build_number = "251.23774.212";
 })
