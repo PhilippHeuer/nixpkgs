d275c0f2e561ae09e2ededfd9c9954ac4b30c272
Philipp Heuer <git@philippheuer.de>
2025-04-23 22:27:34 +0200

    feat: update fuzzmux to v0.10.2

--- pkgs/fuzzmux/default.nix
+++ pkgs/fuzzmux/default.nix
@@ -1,23 +1,24 @@
-{ lib
-, stdenv
-, installShellFiles
-, fetchFromGitHub
-, buildGoModule
-, buildGo123Module
+{
+  lib,
+  stdenv,
+  installShellFiles,
+  fetchFromGitHub,
+  buildGoModule,
+  buildGo124Module,
 }:
 
-buildGo123Module rec {
+buildGo124Module rec {
   # renovate: datasource=github-releases depName=philippheuer/fuzzmux
   pname = "fuzzmux";
-  version = "v0.10.1";
+  version = "v0.10.2";
 
   src = fetchFromGitHub {
     owner = "PhilippHeuer";
     repo = "fuzzmux";
     rev = version;
-    sha256 = "sha256-gtLUXo+jmlTbWfgO/HkFaG3cbADVOmHgVrQUkxlrhUo=";
+    sha256 = "sha256-t7+j2g/ydxVcECw/pHGEdNxcxilMCxEiD3bVF6sA60w=";
   };
-  vendorHash = "sha256-S2PB8+xaqVliNXJaGcflXEZK529WHaqwVEDl6MCM6xY=";
+  vendorHash = "sha256-fUXkpeOR0W/Sqh5OpV2xM2KDsohexeDbkGopecfV5eY=";
 
   ldflags = [
     "-s"
@@ -37,15 +38,15 @@ buildGo123Module rec {
 
   # completions
   postInstall = ''
-      # rename binary
-      mv $out/bin/* $out/bin/tmx
+    # rename binary
+    mv $out/bin/* $out/bin/tmx
 
-      # install shell completion
-      installShellCompletion --cmd tmx \
-        --bash <($out/bin/tmx completion bash) \
-        --fish <($out/bin/tmx completion fish) \
-        --zsh  <($out/bin/tmx completion zsh)
-    '';
+    # install shell completion
+    installShellCompletion --cmd tmx \
+      --bash <($out/bin/tmx completion bash) \
+      --fish <($out/bin/tmx completion fish) \
+      --zsh  <($out/bin/tmx completion zsh)
+  '';
 
   # metadata
   meta = with lib; {
