4389173ef2769c04aac53fac69cecb5b606f585c
Philipp Heuer <git@philippheuer.de>
2025-05-20 23:20:37 +0200

    fix: add buildinputs in goland pkg

--- pkgs/jetbrains/goland-eap/default.nix
+++ pkgs/jetbrains/goland-eap/default.nix
@@ -2,11 +2,17 @@
   pkgs,
 }:
 
-(pkgs.jetbrains.goland.overrideAttrs {
+(pkgs.jetbrains.goland.overrideAttrs(oldAttrs: {
     version = "2025.2 eap";
     src = pkgs.fetchurl {
         url = "https://download-cdn.jetbrains.com/go/goland-252.13776.68.tar.gz";
         sha256 = "8c8d2653020b36ebb115799c92915b4a1f69c6529f0ccf5078e0f9074a0215d6";
     };
     build_number = "252.13776.68";
-})
+
+    buildInputs = (oldAttrs.buildInputs or []) ++ [
+      pkgs.libGL
+      pkgs.xorg.libX11
+      pkgs.fontconfig
+    ];
+}))
