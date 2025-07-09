b60d533c95dc60a190c152ace354cdaf19c534f6
Philipp Heuer <git@philippheuer.de>
2025-07-09 19:55:43 +0200

    feat: update vaccum to v0.17.5

--- pkgs/jetbrains/goland-eap/default.nix
+++ pkgs/jetbrains/goland-eap/default.nix
@@ -3,12 +3,12 @@
 }:
 
 (pkgs.jetbrains.goland.overrideAttrs(oldAttrs: {
-    version = "2025.2 eap";
+    version = "2025.2 EAP 7";
     src = pkgs.fetchurl {
-        url = "https://download-cdn.jetbrains.com/go/goland-252.13776.68.tar.gz";
+        url = "https://download-cdn.jetbrains.com/go/goland-252.23591.30.tar.gz";
         sha256 = "8c8d2653020b36ebb115799c92915b4a1f69c6529f0ccf5078e0f9074a0215d6";
     };
-    build_number = "252.13776.68";
+    build_number = "252.23591.30";
 
     buildInputs = (oldAttrs.buildInputs or []) ++ [
       pkgs.libGL
--- pkgs/jetbrains/idea-ultimate-eap/default.nix
+++ pkgs/jetbrains/idea-ultimate-eap/default.nix
@@ -3,12 +3,12 @@
 }:
 
 (pkgs.jetbrains.idea-ultimate.overrideAttrs(oldAttrs: {
-    version = "2025.2 Beta";
+    version = "2025.2 EAP 7";
     src = pkgs.fetchurl {
-        url = "https://download-cdn.jetbrains.com/idea/ideaIU-252.13776.59.tar.gz";
-        sha256 = "862e7d15b0f8cbd752f969684885a47ffb1e32a2c495089176af92d12164ecad";
+        url = "https://download-cdn.jetbrains.com/idea/ideaIU-252.23591.19.tar.gz";
+        sha256 = "05e469f3c98fb91f780176275e582d5e1d54c48a574e244b07fa3031e1d0997d";
     };
-    build_number = "252.13776.59";
+    build_number = "252.23591.19";
 
     buildInputs = (oldAttrs.buildInputs or []) ++ [
       pkgs.libGL
