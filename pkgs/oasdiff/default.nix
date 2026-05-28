88bbf65e17703ca2df72fcb7a6e0da1f2e3d879b
Philipp Heuer <git@philippheuer.de>
2026-05-28 23:40:20 +0200

    oasdiff: 1.17.0 -> 1.17.0

--- pkgs/oasdiff/default.nix
+++ pkgs/oasdiff/default.nix
@@ -1,28 +1,28 @@
 {
   lib,
-  buildGoModule,
+  buildGo126Module,
   fetchFromGitHub,
   makeWrapper,
   git,
   gitUpdater,
 }:
 
-buildGoModule rec {
+buildGo126Module rec {
   pname = "oasdiff";
-  version = "1.11.7";
+  version = "1.17.0";
 
   src = fetchFromGitHub {
     owner = "oasdiff";
     repo = "oasdiff";
     rev = "v" + version;
-    hash = "sha256-QfTJEwHsjO4ilbU18iDzCCthhb5s2JZRsGIB+SQiBaI=";
+    hash = "sha256-fVxJXivUZsusXNCtb/hyp+TDMO8Zheo3KcVi4zOzjNQ=";
   };
   vendorHash = "sha256-TGkaEH1cExkMJOVJ5iUgnBhb3P9hQhpbC8FIGdtHk5A=";
 
   nativeBuildInputs = [ makeWrapper ];
 
   postInstall = ''
-      wrapProgram $out/bin/oasdiff --prefix PATH : ${lib.makeBinPath [ git ]}
+    wrapProgram $out/bin/oasdiff --prefix PATH : ${lib.makeBinPath [ git ]}
   '';
 
   passthru.updateScript = gitUpdater {
