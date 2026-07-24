6e4a8d77fc0ffdf457dfffa57ecb96d57fb588e9
Philipp Heuer <git@philippheuer.de>
2026-07-24 20:40:33 +0200

    primecodegen: 0.1.2 -> 0.2.0
    Diff: https://github.com/primelib/primecodegen/compare/refs/tags/v0.1.2...v0.2.0

--- pkgs/primelib/primecodegen/default.nix
+++ pkgs/primelib/primecodegen/default.nix
@@ -10,7 +10,7 @@
 buildGoModule rec {
   # renovate: datasource=github-releases depName=primelib/primecodegen
   pname = "primecodegen";
-  version = "0.1.2";
+  version = "0.2.0";
 
   src = fetchFromGitHub {
     owner = "primelib";
